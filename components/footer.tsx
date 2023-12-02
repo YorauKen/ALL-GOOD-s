87c5225dc3955768dca6b908516c13ecb396703a
Hitesh Yadav M <hiteshyadavm007@gmail.com>
2023-12-02 12:27:41 +0530

    store final commit

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+	<footer className="b-white border-t">
+		<div className="mx-auto py-10">
+				<p className="text-center text-xs text-black">
+					&copy; 2023 ALL GOOD's Inc , All rights reserved.
+				</p>
+		</div>
+	</footer>
+  )
+}
+
+export default Footer;
